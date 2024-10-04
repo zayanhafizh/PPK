4cf6ce60d4f59ba991d5523ac1f667b80e73cd4c
zayanhafizh <zayan54562@gmail.com>
2024-10-04 13:06:49 +0700

    fix tugas

---
+++ Pertemuan 2/xmlrpc/src/main/java/com/example/xmlrpc/XmlrpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlrpcApplication.class, args);
+	}
+
+}
---
+++ Pertemuan 2/xmlrpc/src/test/java/com/example/xmlrpc/XmlrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmlrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
